37718077612e96af10294a7e71a9e3c350d242f0
DSoLetsDev <DSO@NC055498>
2021-01-20 08:27:45 -0800

    removed references to fetchMaintenanceTypes, fetchUnitofMeasures, fetchFeatureTypes

--- client/src/js/redux/reducers/codeLookupsReducer.js
+++ client/src/js/redux/reducers/codeLookupsReducer.js
@@ -1,11 +1,8 @@
 import _ from 'lodash';
 
-import { FETCH_MAINTENANCE_TYPES, FETCH_UNIT_OF_MEASURES, FETCH_FEATURE_TYPES } from '../actions/types';
+import {} from '../actions/types';
 
 const defaultState = {
-  maintenanceTypes: [],
-  unitOfMeasures: [],
-  featureTypes: [],
   locationCodes: [],
   activityCodes: [],
   thresholdLevels: [],
@@ -16,12 +13,6 @@ const defaultState = {
 
 const codeLookupsReducer = (state = defaultState, action) => {
   switch (action.type) {
-    case FETCH_MAINTENANCE_TYPES:
-      return { ...state, maintenanceTypes: _.orderBy(action.payload, ['name']) };
-    case FETCH_UNIT_OF_MEASURES:
-      return { ...state, unitOfMeasures: _.orderBy(action.payload, ['name']) };
-    case FETCH_FEATURE_TYPES:
-      return { ...state, featureTypes: _.orderBy(action.payload, ['name']) };
     default:
       return state;
   }
