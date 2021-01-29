0b5c858e0dd9aeac84ebc1f330bc09c335eddd9d
Young-Jin Chung <young-jin.chung@bc.gov.ca>
2021-01-29 14:50:32 -0800

    Proj search dto change to support sort and display

--- api/Crt.Model/Dtos/Project/ProjectSearchDto.cs
+++ api/Crt.Model/Dtos/Project/ProjectSearchDto.cs
@@ -9,9 +9,12 @@ namespace Crt.Model.Dtos.Project
         public decimal ProjectId { get; set; }
         public decimal RegionNumber { get; set; }
         public string RegionName { get; set; }
+        [JsonPropertyName("number")]
         public string ProjectNumber { get; set; }
         public string ProjectName { get; set; }
         public DateTime? EndDate { get; set; }
         public bool IsInProgress { get => EndDate == null || DateTime.Today < EndDate; }
+        [JsonPropertyName("projectNumber")]
+        public string Name { get => $"{ProjectNumber}-{ProjectName}"; }
     }
 }
