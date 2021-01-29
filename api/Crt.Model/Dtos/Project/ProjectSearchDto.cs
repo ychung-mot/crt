da4e9c2ec218e36cc7f77fcb62fa945119b0ed39
Young-Jin Chung <young-jin.chung@bc.gov.ca>
2021-01-29 15:12:11 -0800

    Prj search dto field name changes to support sort

--- api/Crt.Model/Dtos/Project/ProjectSearchDto.cs
+++ api/Crt.Model/Dtos/Project/ProjectSearchDto.cs
@@ -7,14 +7,21 @@ namespace Crt.Model.Dtos.Project
     {
         [JsonPropertyName("id")]
         public decimal ProjectId { get; set; }
+        [JsonIgnore]
         public decimal RegionNumber { get; set; }
+        [JsonIgnore]
         public string RegionName { get; set; }
-        [JsonPropertyName("number")]
+        [JsonIgnore]
         public string ProjectNumber { get; set; }
+        [JsonIgnore]
         public string ProjectName { get; set; }
+
+        [JsonPropertyName("regionId")]
+        public string regionField { get => $"{RegionNumber}-{RegionName}"; }
+        [JsonPropertyName("projectNumber")]
+        public string projectField { get => $"{ProjectNumber}-{ProjectName}"; }
         public DateTime? EndDate { get; set; }
         public bool IsInProgress { get => EndDate == null || DateTime.Today < EndDate; }
-        [JsonPropertyName("projectNumber")]
-        public string Name { get => $"{ProjectNumber}-{ProjectName}"; }
+
     }
 }
