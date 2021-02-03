90cb766534ff3519c33029c94c49d420a06c1912
Young-Jin Chung <young-jin.chung@bc.gov.ca>
2021-02-03 14:56:21 -0800

    New Dtos and code look up

---
+++ api/Crt.Model/Dtos/Element/ElementDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Crt.Model.Dtos.Element
+{
+    public class ElementDto
+    {
+        [JsonPropertyName("id")]
+        public decimal ElementId { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string Comment { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
---
+++ api/Crt.Model/Dtos/FinTarget/FinTargetDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Crt.Model.Dtos.FinTarget
+{
+    public class FinTargetDto
+    {
+        [JsonPropertyName("id")]
+        public decimal FinTargetId { get; set; }
+        public decimal ProjectId { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public decimal FiscalYearLkupId { get; set; }
+        public decimal ElementId { get; set; }
+        public decimal PhaseLkupId { get; set; }
+        public decimal ForecastTypeLkupId { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
