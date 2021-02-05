fa8372179a8789a0c864fdd5e215c0c24f099905
Young-Jin Chung <young-jin.chung@bc.gov.ca>
2021-02-05 13:33:27 -0800

    Element APIs

---
+++ api/Crt.Data/Repositories/ElementRepository.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Crt.Data.Database.Entities;
+using Crt.Data.Repositories.Base;
+using Crt.Model;
+using Crt.Model.Dtos.Element;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crt.Data.Repositories
+{
+    public interface IElementRepository
+    {
+        Task<IEnumerable<ElementDto>> GetElementsAsync();
+    }
+
+    public class ElementRepository : CrtRepositoryBase<CrtElement>, IElementRepository
+    {
+        public ElementRepository(AppDbContext dbContext, IMapper mapper, CrtCurrentUser currentUser)
+            : base(dbContext, mapper, currentUser)
+        {
+        }
+
+        public async Task<IEnumerable<ElementDto>> GetElementsAsync()
+        {
+            return await GetAllNoTrackAsync<ElementDto>(x => x.EndDate == null || x.EndDate > DateTime.Today);
+        }
+    }
+}
---
+++ api/Crt.Domain/Services/ElementService.cs
@@ -0,0 +1,31 @@
+﻿using Crt.Data.Database;
+using Crt.Data.Repositories;
+using Crt.Domain.Services.Base;
+using Crt.Model;
+using Crt.Model.Dtos.Element;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Crt.Domain.Services
+{
+    public interface IElementService
+    {
+        Task<IEnumerable<ElementDto>> GetElementsAsync();
+    }
+
+    public class ElementService : CrtServiceBase, IElementService
+    {
+        private IElementRepository _elementRepo;
+
+        public ElementService(CrtCurrentUser currentUser, IFieldValidatorService validator, IUnitOfWork unitOfWork, IElementRepository elementRepo)
+            : base(currentUser, validator, unitOfWork)
+        {
+            _elementRepo = elementRepo;
+        }
+
+        public async Task<IEnumerable<ElementDto>> GetElementsAsync()
+        {
+            return await _elementRepo.GetElementsAsync();
+        }
+    }
+}
