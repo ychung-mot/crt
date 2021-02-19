bf7e23b9b1dc14d2dd2935da6a4f9b9c350db518
Darrel Siegle <darrel.siegle@dxcas.com>
2021-02-19 08:35:12 -0800

    CRPDB-115 - Updated CodeLookup controller, forecasttype is now fundingtype - updated CodeSet constant, forecasttype is now fundingtype

--- api/Crt.Model/Constants.cs
+++ api/Crt.Model/Constants.cs
@@ -131,7 +131,7 @@ namespace Crt.Model
         public const string Quantity = "QUANTITY";
         public const string Accomplishment = "ACCOMPLISHMENT";
         public const string Contractor = "CONTRACTOR";
-        public const string ForecastType = "FORECAST_TYPE";
+        public const string FundingType = "FUNDING_TYPE";
     }
 
     public static class CrtEnvironments
