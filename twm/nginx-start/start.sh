3c4e2dcd11c31ae83350891a7af3ff6b6b50fc3d
Young-Jin Chung <young-jin.chung@bc.gov.ca>
2021-03-02 07:56:41 -0800

    test

--- twm/nginx-start/start.sh
+++ twm/nginx-start/start.sh
@@ -3,5 +3,5 @@
 echo "---> Creating keycloak.json ..."
 envsubst < ~/keycloak.json.tmpl > ~/keycloak.json
 
-echo "---> Creating nginx.conf ..."
-envsubst '${CRT_DEPLOY_SUFFIX}' < /tmp/src/nginx.conf.tmpl > /etc/nginx/nginx.conf
+#echo "---> Creating nginx.conf ..."
+#envsubst '${CRT_DEPLOY_SUFFIX}' < /tmp/src/nginx.conf.tmpl > /etc/nginx/nginx.conf
