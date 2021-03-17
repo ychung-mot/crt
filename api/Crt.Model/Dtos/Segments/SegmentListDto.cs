584e91dc61fb76d32d72a9ead9264e674ba310ac
Darrel Siegle <darrel.siegle@dxcas.com>
2021-03-17 10:32:20 -0700

    Added formatting to coordinates to only return 6 points after the decimal for Lat/Long

--- api/Crt.Model/Dtos/Segments/SegmentListDto.cs
+++ api/Crt.Model/Dtos/Segments/SegmentListDto.cs
@@ -7,7 +7,7 @@ namespace Crt.Model.Dtos.Segments
         [JsonPropertyName("id")]
         public decimal SegmentId { get; set; }
         public bool CanDelete { get => true; }
-        public string StartCoordinates { get => $"{StartLatitude},{StartLongitude}"; }
-        public string EndCoordinates { get => $"{EndLatitude},{EndLongitude}"; }
+        public string StartCoordinates { get => $"{string.Format("{0:N6}", StartLatitude)},{string.Format("{0:N6}", StartLongitude)}"; }
+        public string EndCoordinates { get => $"{string.Format("{0:N6}", EndLatitude)},{string.Format("{0:N6}", EndLongitude)}"; }
     }
 }
