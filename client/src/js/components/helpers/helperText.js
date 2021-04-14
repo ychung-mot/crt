c87f10e8a0d4e4c31f918b0868af1d3cda5dbcba
DSoLetsDev <DSO@NC055498>
2021-04-14 08:53:06 -0700

    changed helper text for project completion (endDate)

--- client/src/js/components/helpers/helperText.js
+++ client/src/js/components/helpers/helperText.js
@@ -8,7 +8,7 @@ export const HELPER_TEXT = {
   rcLkupId: `Financial Code`,
   projectMgrLkupId: `Key contact for the project`,
   nearstTwnLkupId: `Used to report on project outcomes`,
-  endDate: `Checked means project is complete`,
+  endDate: `Is project complete`,
   tenderNumber: `Identifier for the tender <project#>-####`,
   plannedDate: `Planned date for posting Tender at Provincial Contracts`,
   actualDate: `Actual date for posting Tender at Provincial Contracts`,
